02d2173dceba75bf3dce0c06f98022c160d0de19
zplusfour <zgjoniur@gmail.com>
2024-01-04 13:25:03 +0200

    score counter feature

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
-import { GetServerSideProps } from "next";
-import { IATA_MAP, IATAS, IMAGES_MAP, AIRLINES } from "../utils/iata";
 import { randomize } from "../utils/utils";
+import { useState, useEffect } from "react";
 
 const shuffleArray = (array: string[]) => {
   for (let i = array.length - 1; i > 0; i--) {
@@ -11,39 +10,54 @@ const shuffleArray = (array: string[]) => {
 };
 
 export default function Home({ answer, image, options }: any) {
+  const [score, setScore] = useState<number>(0);
+
+  useEffect(() => {
+    const scoreStr = localStorage.getItem('score');
+    const parsedScore = scoreStr ? parseInt(scoreStr) : 0;
+    setScore(parsedScore);
+  }, []);
+
   const handleClick = (selectedOption: string) => {
     if (selectedOption === answer) {
-      alert("Correct!");
-      window.location.reload();
+      alert('Correct!');
+      const currentScore = score + 1;
+      setScore(currentScore);
+      localStorage.setItem('score', currentScore.toString());
     } else {
-      alert("Incorrect!");
-      window.location.reload();
+      alert('Incorrect!');
     }
+    window.location.reload();
   };
+
   return (
     <div
-      style={{ justifyContent: "center", textAlign: "center", margin: "200px" }}
+      style={{
+        justifyContent: 'center',
+        textAlign: 'center',
+        margin: '200px',
+      }}
     >
       <h1>Guess the Airline</h1>
-      <br />
       <p>
-        <img src={image}></img>
+        <img src={image} alt="Airline" />
       </p>
+      <h2>Score: {score}</h2>
       {options.map((option: string, index: number) => (
         <li
           key={index}
           style={{
-            cursor: "pointer",
-            listStyle: "none",
-            margin: "5px",
-            padding: "10px",
-            border: "1px solid #ccc",
-            borderRadius: "5px",
-            backgroundColor: "#f0f0f0",
-            textAlign: "center",
-            fontWeight: "bold",
-            textTransform: "uppercase",
-            color: "#333",
+            cursor: 'pointer',
+            listStyle: 'none',
+            margin: '5px',
+            padding: '10px',
+            border: '1px solid #ccc',
+            borderRadius: '5px',
+            backgroundColor: '#f0f0f0',
+            textAlign: 'center',
+            fontWeight: 'bold',
+            textTransform: 'uppercase',
+            color: '#333',
           }}
           onClick={() => handleClick(option)}
         >
